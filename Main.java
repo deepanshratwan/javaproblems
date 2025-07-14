83bc83c55afff34e841e5b5161643ec843e08681
deepanshratwan <deepanshratwan@gmail.com>
2025-07-14 17:42:11 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ public class Main {
         }
         int maxSum =Integer.MIN_VALUE;// to get the minimum value
 //the range of i taken to get consecutive sum of the array like here n is 4 k is 2 we want to get i  move from  0 to 1 so n-k is good
-         for (int i = 0; i < n-k+1; i++) {
+         for (int i = 0; i < n-k+1; i++) {//we can also shose i<=n-k
              int sum=0;// it can store the sum
              for (int j = 0; j < k; j++) {//to move j from 0 to 1
                  sum+=arr[i+j];//the value changes w.r.t i
