c21b397648dcb66918a7c9d9aac4e7b460f2e62c
deepanshratwan <deepanshratwan@gmail.com>
2025-07-15 17:39:39 +0530

    Add files via upload
    it is based on the concept to find the ways of arranging the objects in a row

--- Main.java
+++ Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static int maxSum(int[]arr,int k,int n){
         if(k>n){
             System.out.println("invalid!");
-            return 0;
+            return -1;//if k is greater than n then it is invalid
         }
         int maxSum =Integer.MIN_VALUE;// to get the minimum value
 //the range of i taken to get consecutive sum of the array like here n is 4 k is 2 we want to get i  move from  0 to 1 so n-k is good
-         for (int i = 0; i < n-k+1; i++) {//we can also shose i<=n-k
+         for (int i = 0; i < n-k+1; i++) {
              int sum=0;// it can store the sum
              for (int j = 0; j < k; j++) {//to move j from 0 to 1
                  sum+=arr[i+j];//the value changes w.r.t i
@@ -26,7 +26,7 @@ public class Main {
     }
     public static void main(String[] args) {
 
-    int[] arr ={100,200,300,400};
+    int[] arr ={100,200,300,400,500};
     int k=2;
     int n=arr.length;
         System.out.println(maxSum(arr,k,n));
